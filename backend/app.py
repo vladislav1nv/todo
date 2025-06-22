e17c4f4994c84867fbb6ed261c888ea123f02cd1
vladislav1nv <ryzhov-0105@mail.ru>
2025-06-22 18:03:37 +0500

    add dir frontend and set routes

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 import sqlite3
 from flask_cors import CORS
-from flask import send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='frontend', static_url_path='')
 CORS(app)
 
 def init_db():
@@ -56,8 +56,8 @@ if __name__ == '__main__':
 
 @app.route('/')
 def serve_index():
-    return send_from_directory('../frontend', 'index.html')
+    return send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/<path:path>')
 def serve_static(path):
-    return send_from_directory('../frontend', path)
+    return send_from_directory(app.static_folder, path)
